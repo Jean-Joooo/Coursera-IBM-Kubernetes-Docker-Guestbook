d76a272e9ac45214339c5e727b0f633794012444
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2023-12-28 22:39:42 +0100

    Add files via upload

---
+++ router/general.js
@@ -0,0 +1,38 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Get the book list available in the shop
+public_users.get("/",(req,res)=>{
+    res.send(JSON.stringify(friends,null,4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+        res.send(JSON.stringify(friends,null,4));
+    });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    res.send(JSON.stringify(friends,null,4));
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    res.send(JSON.stringify(friends,null,4));
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+    res.send(JSON.stringify(friends,null,4));
+});
+
+module.exports.general = public_users;
