ce28ffd8d5e375f2588c7cc9b2a0742e1969016e
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-10 11:04:24 +0100

    Delete scripts directory

--- scripts/generate-jwt-secret.js
+++
@@ -1,8 +0,0 @@
-// generate-jwt-secret.js
-const crypto = require("crypto");
-require("dotenv").config();
-
-// Génération d'une clé secrète forte de 64 caractères
-const secretKey = crypto.randomBytes(32).toString("hex");
-
-console.log("Generated JWT Secret Key:", secretKey);
